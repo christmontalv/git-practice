8e19fe53366e9d95fbd6f7dbad1738506a201927
christmontalv <christian.987.cma@gmail.com>
2024-09-07 01:22:29 -0300

    cambiando ultima linea login.js

--- login.js
+++ login.js
@@ -4,4 +4,4 @@ const validateUser = (username, password) => {
 
 console.log(`User ${validateUser('hehe', 'password') ? 'is valid':'is not valid'}`);
 
-console.log('new login heheh')
+console.log('login heheh')
